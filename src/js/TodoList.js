10cab2533f1957c95eeed8caddf2e16842970d2b
Loris <loris4526@gmail.com>
2017-11-23 11:04:39 +0100

    Allow 'TodoList' component to change 'filter' in MobX store

--- src/js/TodoList.js
+++ src/js/TodoList.js
@@ -4,15 +4,22 @@ import {observer} from 'mobx-react'
 
 @observer export default class TodoList extends Component {
 
+  filter(e){
+    this.props.store.filter = e.target.value
+  }
+
   render(){
-    const {todos} = this.props.store
+    const {todos, filter} = this.props.store
     const todoLis = todos.map(todo => (
-      <li>{todo}</li>
+      <li key={todo}>{todo}</li>
     ))
-    
+
     return (
       <div>
         <h1>Todos:</h1>
+
+        <div>{filter}</div>
+        <input className="filter" value={filter} onChange={this.filter.bind(this)}/>
 
         <ul>{todoLis}</ul>
       </div>
